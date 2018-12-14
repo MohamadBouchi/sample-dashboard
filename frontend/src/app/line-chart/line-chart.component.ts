951a6d8756680faefd2154862b84479d5d41f5d5
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2018-12-14 11:01:05 +0100

    pipe and services folders added

--- frontend/src/app/line-chart/line-chart.component.ts
+++ frontend/src/app/line-chart/line-chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ng2DeviceService } from 'ng2-device-detector';
-import { BadgeListsService } from '../badge-lists.service';
+import { BadgeListsService } from '../../../services/badge-lists.service';
 
 @Component({
   selector: 'app-line-chart',
