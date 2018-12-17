5dd6830760326ee30639794527f060a71f8f8256
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2018-12-17 11:36:31 +0100

    search in user table by first name or last name

--- frontend/src/app/pipes/table-search.pipe.ts
+++ frontend/src/app/pipes/table-search.pipe.ts
@@ -9,8 +9,9 @@ export class TableSearchPipe implements PipeTransform {
   transform(users: any[], searchValue: String): any {
     if (!searchValue) { return users; }
     return users.filter(user => {
-      if (user.first_name !== undefined) {
-        return user.first_name.toLocaleLowerCase().match(searchValue.toLowerCase());
+      if (user.first_name !== undefined || user.last_name !== undefined) {
+        return user.last_name.toLocaleLowerCase().match(searchValue.toLowerCase()) ||
+        user.first_name.toLocaleLowerCase().match(searchValue.toLowerCase());
       }
     });
   }
